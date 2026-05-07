58a65e048cebd4f1a044fc3ae1f2a7b48ccae721
xpdrengen-beep <xpdrengen@gmail.com>
2026-05-07 14:14:52 +0200

    Update LootTables.c

--- LootTables.c
+++ LootTables.c
@@ -1,5 +1,6 @@
 class LootTables
 {
+	
 	static ref array<ref LootEntry> h07BedLoot = {
 		new LootEntry("{4B57C11AA5161760}Prefabs/Characters/Vests/Vest_PASGT/Vest_PASGT.et", 60),
 
@@ -33,12 +34,21 @@ class LootTables
 		new LootEntry("{AC298BA8A7D27007}Prefabs/Items/Equipment/Backpacks/Backpack_Medical_Soviet_NoCross_01.et", 34),
 		new LootEntry("{6A39B5843B3F36DA}Prefabs/Items/Equipment/Backpacks/Backpack_RPG_Assistant.et", 15)
 	};
+	
+	static ref array<ref LootEntry> h05BedLoot = {
+		new LootEntry("{4B57C11AA5161760}Prefabs/Characters/Vests/Vest_PASGT/Vest_PASGT.et", 60),
 
-	static ref array<ref LootEntry> h04KitchenItems = {
+		new LootEntry("{06B68C58B72EAAC6}Prefabs/Items/Equipment/Backpacks/Backpack_ALICE_Medium.et", 15),
+		new LootEntry("{95D4766BBE46F23D}Prefabs/Items/Equipment/Backpacks/Backpack_IIFS_FieldPack.et", 25),
+		new LootEntry("{AC298BA8A7D27007}Prefabs/Items/Equipment/Backpacks/Backpack_Medical_Soviet_NoCross_01.et", 34),
+		new LootEntry("{6A39B5843B3F36DA}Prefabs/Items/Equipment/Backpacks/Backpack_RPG_Assistant.et", 15)
+	};
+
+	static ref array<ref LootEntry> h05KitchenItems = {
 		new LootEntry("{128A19AA9F731095}Prefabs/Items/Food/MeatLeg_01.et", 65)
 	};
 
-	static ref array<ref LootEntry> h04WeaponItems = {
+	static ref array<ref LootEntry> h05WeaponItems = {
 		new LootEntry("{C1B4F7B87B720EE8}Prefabs/Weapons/Rifles/Mosin/BC_Rifle_Mosin_Nagant_9130.et", 15),
 		new LootEntry("{6545DEABED4759E5}Prefabs/Weapons/Rifles/Mosin/BC_Rifle_Mosin_Nagant_9130_PU.et", 5),
 		new LootEntry("{8F5A4A44DD1D84D6}Prefabs/Weapons/Rifles/M1 Garand/BC_Rifle_M1_Garand.et", 15),
@@ -46,7 +56,7 @@ class LootTables
 		new LootEntry("{8231579E96735D40}Prefabs/Weapons/Attachments/Muzzle/Mosin/BC_Suppressor_Bramit_Mosin.et", 3)
 	};
 
-	static ref array<ref LootEntry> h04MedicalItems = {
+	static ref array<ref LootEntry> h05MedicalItems = {
 		new LootEntry("{AC298BA8A7D27007}Prefabs/Items/Equipment/Backpacks/Backpack_Medical_Soviet_NoCross_01.et", 100)
 	};
 

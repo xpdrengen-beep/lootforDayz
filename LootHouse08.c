88b778d1b2361faeffb2d7dcdae76db854763a68
xpdrengen-beep <xpdrengen@gmail.com>
2026-05-08 14:09:50 +0200

    Refactor loot arrays for Enfusion compile

--- LootHouse08.c
+++ LootHouse08.c
@@ -1,44 +1,74 @@
 class LootHouse08
 {
 	static string PREFAB = "{1CB98E016E11635B}Prefabs/Structures/Houses/Village/Single Floor Buildings/House 08.et";
+	static ref array<vector> BedSpawns;
 
-	static ref array<vector> BedSpawns = {
-		"-5.197 0.715 0.574",
-		"-5.093 0.715 -0.906",
-		"-4.021 0.725 -3.624",
-		"-5.496 0.721 -3.323",
-		"-5.013 4.348 0.957",
-		"-5.551 4.348 2.651"
-	};
+	static array<vector> GetBedSpawns()
+	{
+		if (!BedSpawns)
+		{
+			BedSpawns = new array<vector>();
+			BedSpawns.Insert("-5.197 0.715 0.574");
+			BedSpawns.Insert("-5.093 0.715 -0.906");
+			BedSpawns.Insert("-4.021 0.725 -3.624");
+			BedSpawns.Insert("-5.496 0.721 -3.323");
+			BedSpawns.Insert("-5.013 4.348 0.957");
+			BedSpawns.Insert("-5.551 4.348 2.651");
+		}
 
-	static ref array<vector> KitchenSpawns = {
-		"2.699 1.133 3.507",
-		"1.75 1.133 3.113",
-		"-0.882 1.145 1.34"
-	};
+		return BedSpawns;
+	}
+	static ref array<vector> KitchenSpawns;
 
-	static ref array<vector> WeaponSpawns = {
-		"-5.689 0.97 -2.633",
-		"-2.29 4.325 1.556",
-		"2.323 3.969 2.514"
-	};
+	static array<vector> GetKitchenSpawns()
+	{
+		if (!KitchenSpawns)
+		{
+			KitchenSpawns = new array<vector>();
+			KitchenSpawns.Insert("2.699 1.133 3.507");
+			KitchenSpawns.Insert("1.75 1.133 3.113");
+			KitchenSpawns.Insert("-0.882 1.145 1.34");
+		}
 
-	static ref array<vector> MedicalSpawns = {
-		"-2.802 1.013 4.744",
-		"-4.477 4.589 2.54",
-		"3.056 4.34 2.52",
-		"2.349 4.347 2.583",
-		"4.738 4.397 0.675"
-	};
+		return KitchenSpawns;
+	}
+	static ref array<vector> WeaponSpawns;
 
+	static array<vector> GetWeaponSpawns()
+	{
+		if (!WeaponSpawns)
+		{
+			WeaponSpawns = new array<vector>();
+			WeaponSpawns.Insert("-5.689 0.97 -2.633");
+			WeaponSpawns.Insert("-2.29 4.325 1.556");
+			WeaponSpawns.Insert("2.323 3.969 2.514");
+		}
+
+		return WeaponSpawns;
+	}
+	static ref array<vector> MedicalSpawns;
+
+	static array<vector> GetMedicalSpawns()
+	{
+		if (!MedicalSpawns)
+		{
+			MedicalSpawns = new array<vector>();
+			MedicalSpawns.Insert("-2.802 1.013 4.744");
+			MedicalSpawns.Insert("-4.477 4.589 2.54");
+			MedicalSpawns.Insert("3.056 4.34 2.52");
+			MedicalSpawns.Insert("2.349 4.347 2.583");
+			MedicalSpawns.Insert("4.738 4.397 0.675");
+		}
+
+		return MedicalSpawns;
+	}
 	static void Spawn(IEntity house, SpawnedHouseLoot data)
 	{
 		if (!house || !data)
 			return;
-
-		LootSpawner.SpawnWeightedFromArray(house, data, BedSpawns, LootTables.h08BedLoot, 0.5, "BED", "0 0 0", 0.01);
-		LootSpawner.SpawnWeightedFromArray(house, data, KitchenSpawns, LootTables.h08KitchenItems, 0.5, "KITCHEN", "0 0 0", 0.01);
-		LootSpawner.SpawnWeightedFromArray(house, data, WeaponSpawns, LootTables.h08WeaponItems, 0.1, "WEAPON", "0 0 0", 0.02);
-		LootSpawner.SpawnWeightedFromArray(house, data, MedicalSpawns, LootTables.h08MedicalItems, 0.35, "MEDICAL", "0 0 0", 0.01);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetBedSpawns(), LootTables.Geth08BedLoot(), 0.5, "BED", "0 0 0", 0.01);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetKitchenSpawns(), LootTables.Geth08KitchenItems(), 0.5, "KITCHEN", "0 0 0", 0.01);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetWeaponSpawns(), LootTables.Geth08WeaponItems(), 0.1, "WEAPON", "0 0 0", 0.02);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetMedicalSpawns(), LootTables.Geth08MedicalItems(), 0.35, "MEDICAL", "0 0 0", 0.01);
 	}
 }
--- LootHouse_WoodenE1I01_P.c
+++ LootHouse_WoodenE1I01_P.c
@@ -1,38 +1,68 @@
 class LootHouse_WoodenE1I01_P
 {
 	static string PREFAB = "{FD91510268A541A7}PrefabLibrary/DefaultLibrary/Structures/Houses/House_Wooden_E_1I01/House_Wooden_E_1I01_panels.et";
+	static ref array<vector> BedSpawns;
 
-	static ref array<vector> BedSpawns = {
-		"3.362 1.658 2.011",
-		"3.662 1.864 1.201"
-	};
+	static array<vector> GetBedSpawns()
+	{
+		if (!BedSpawns)
+		{
+			BedSpawns = new array<vector>();
+			BedSpawns.Insert("3.362 1.658 2.011");
+			BedSpawns.Insert("3.662 1.864 1.201");
+		}
 
-	static ref array<vector> KitchenSpawns = {
-		"-0.952 2.062 -2.256",
-		"-0.062 2.062 -2.055",
-		"-2.508 2.085 -0.768",
-		"-3.281 2.182 -0.501",
-		"-3.968 2.181 -0.636"
-	};
+		return BedSpawns;
+	}
+	static ref array<vector> KitchenSpawns;
 
-	static ref array<vector> WeaponSpawns = {
-		"1.463 2.173 -2.464"
-	};
+	static array<vector> GetKitchenSpawns()
+	{
+		if (!KitchenSpawns)
+		{
+			KitchenSpawns = new array<vector>();
+			KitchenSpawns.Insert("-0.952 2.062 -2.256");
+			KitchenSpawns.Insert("-0.062 2.062 -2.055");
+			KitchenSpawns.Insert("-2.508 2.085 -0.768");
+			KitchenSpawns.Insert("-3.281 2.182 -0.501");
+			KitchenSpawns.Insert("-3.968 2.181 -0.636");
+		}
 
-	static ref array<vector> MedicalSpawns = {
-		"-3.705 2.304 2.704",
-		"-4.855 2.02 2.186",
-		"-4.587 1.173 0.676"
-	};
+		return KitchenSpawns;
+	}
+	static ref array<vector> WeaponSpawns;
 
+	static array<vector> GetWeaponSpawns()
+	{
+		if (!WeaponSpawns)
+		{
+			WeaponSpawns = new array<vector>();
+			WeaponSpawns.Insert("1.463 2.173 -2.464");
+		}
+
+		return WeaponSpawns;
+	}
+	static ref array<vector> MedicalSpawns;
+
+	static array<vector> GetMedicalSpawns()
+	{
+		if (!MedicalSpawns)
+		{
+			MedicalSpawns = new array<vector>();
+			MedicalSpawns.Insert("-3.705 2.304 2.704");
+			MedicalSpawns.Insert("-4.855 2.02 2.186");
+			MedicalSpawns.Insert("-4.587 1.173 0.676");
+		}
+
+		return MedicalSpawns;
+	}
 	static void Spawn(IEntity house, SpawnedHouseLoot data)
 	{
 		if (!house || !data)
 			return;
-
-		LootSpawner.SpawnWeightedFromArray(house, data, BedSpawns, LootTables.E1I01_PBedLoot, 0.5, "BED", "0 0 0", 0.01);
-		LootSpawner.SpawnWeightedFromArray(house, data, KitchenSpawns, LootTables.E1I01_PKitchenItems, 0.5, "KITCHEN", "0 0 0", 0.01);
-		LootSpawner.SpawnWeightedFromArray(house, data, WeaponSpawns, LootTables.E1I01_PWeaponItems, 0.1, "WEAPON", "90 0 90", 0.02);
-		LootSpawner.SpawnWeightedFromArray(house, data, MedicalSpawns, LootTables.E1I01_PMedicalItems, 0.35, "MEDICAL", "0 0 0", 0.01);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetBedSpawns(), LootTables.GetE1I01_PBedLoot(), 0.5, "BED", "0 0 0", 0.01);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetKitchenSpawns(), LootTables.GetE1I01_PKitchenItems(), 0.5, "KITCHEN", "0 0 0", 0.01);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetWeaponSpawns(), LootTables.GetE1I01_PWeaponItems(), 0.1, "WEAPON", "90 0 90", 0.02);
+		LootSpawner.SpawnWeightedFromArray(house, data, GetMedicalSpawns(), LootTables.GetE1I01_PMedicalItems(), 0.35, "MEDICAL", "0 0 0", 0.01);
 	}
 }
